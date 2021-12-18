fc6177bd56c5faead76af497dba9c6f2c196d8a3
Federico Matos <80070983+federicomatosm@users.noreply.github.com>
2021-12-17 20:52:21 -0400

    RRW-1- Crate Car

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Domain.Entities
+{
+	public class Car
+	{
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MelFunctionChance { get; set; }
+        public int MelFunctionsOccured { get; set; }
+        public int DistanceConverInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+       
+
+    }
+}
+
